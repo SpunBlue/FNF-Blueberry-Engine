be05c81897beb8f319dcbc41c8f96f09147ec3e3
Gidk-g <gidkg302@gmail.com>
2023-01-30 15:35:25 +0100

    custom note script

--- source/Note.hx
+++ source/Note.hx
@@ -73,7 +73,15 @@ class Note extends FlxSprite
 				noteJson = Json.parse(File.getContent(Paths.json("notes/" + specialType)));
 			}
 		}
-		
+
+		if (FileSystem.exists(Modding.getFilePath(specialType + '.hx', "scripts/notes/"))){
+			PlayState.instance.script.loadScript("notes/" + specialType, true);
+		}
+
+		if (Assets.exists(Paths.hx("scripts/notes/" + specialType))){
+			PlayState.instance.script.loadScript("scripts/notes/" + specialType, false);
+		}
+
 		if (noteJson != null && noteJson.image != null && noteJson.xml != null){
 			var spriteAntialiasing:Bool = true;
 			var spriteScale:Float = 0;
